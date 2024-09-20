8eb82a83da76e43a2b1d6cd6904776c18b84cbe4
Michal <michal@drontrzebownisko.pl>
2024-09-20 11:50:54 +0200

    Chat history functionality was added, and its operation was tested using static data.

--- server/src/services/chatService.ts
+++ server/src/services/chatService.ts
@@ -1,15 +1,18 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
 import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
-import { OpenAIEmbeddings } from "@langchain/openai";
 import {
   RunnableSequence,
   RunnablePassthrough,
 } from "@langchain/core/runnables";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 import { formatDocumentsAsString } from "langchain/util/document";
+import { AIMessage, HumanMessage } from "@langchain/core/messages";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -42,13 +45,14 @@ const loadVectorStore = async (): Promise<HNSWLib> => {
 };
 
 const qaSystemPrompt = `You are an assistant for question-answering tasks.
-Use the following pieces of retrieved context to answer the question.
-If you don't know the answer, just say that you don't know.
-
-{context}`;
+  Use the following pieces of retrieved context to answer the question.
+  If you don't know the answer, just say that you don't know.
+  
+  {context}`;
 
 const qaPrompt = ChatPromptTemplate.fromMessages([
   ["system", qaSystemPrompt],
+  new MessagesPlaceholder("chat_history"),
   ["human", "{question}"],
 ]);
 
@@ -75,14 +79,32 @@ const chatService = async (): Promise<void> => {
 
     const ragChain = createRagChain(retriever);
 
-    const question =
+    let chat_history: Array<HumanMessage | AIMessage> = [];
+
+    const question1 =
       "Testuję aplikację i korzystam z dwóch urządzeń. Gdy zakupię aplikację co się stanie?";
 
-    const response = await ragChain.invoke({
-      question,
+    const response1 = await ragChain.invoke({
+      question: question1,
+      chat_history: chat_history,
     });
 
-    console.log("Asystent:", response);
+    console.log("Asystent:", response1);
+
+    chat_history.push(new HumanMessage(question1));
+    chat_history.push(new AIMessage(response1));
+
+    const question2 = "Co z moimi danymi?";
+
+    const response2 = await ragChain.invoke({
+      question: question2,
+      chat_history: chat_history,
+    });
+
+    console.log("Asystent:", response2);
+
+    chat_history.push(new HumanMessage(question2));
+    chat_history.push(new AIMessage(response2));
   } catch (error) {
     console.error("Błąd podczas obsługi zapytania:", error);
   }
