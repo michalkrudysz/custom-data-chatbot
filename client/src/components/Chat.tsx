351cc30d08a02d07f2188cf54718bf2e5e7fdfb7
Michal <michal@drontrzebownisko.pl>
2024-09-25 16:25:46 +0200

    Developed improved logic for the chat, added animations, and styled it appropriately to ensure it is visually appealing.

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,16 +1,37 @@
 import { useState, useEffect } from "react";
 import { chatService, ChatMessage } from "../api/client";
+import classes from "./Chat.module.scss";
+import sendIcon from "../assets/send_icon.svg";
+
+interface ExtendedChatMessage extends ChatMessage {
+  isTyping?: boolean;
+}
 
 const Chat: React.FC = () => {
   const [message, setMessage] = useState<string>("");
-  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
+  const [chatMessages, setChatMessages] = useState<ExtendedChatMessage[]>([]);
 
   useEffect(() => {
     const handleResponse = (data: string) => {
-      setChatMessages((prevMessages) => [
-        ...prevMessages,
-        { sender: "server", message: data },
-      ]);
+      setChatMessages((prevMessages) => {
+        const updatedMessages = [...prevMessages];
+        const typingMessageIndex = updatedMessages.findIndex(
+          (msg) => msg.isTyping === true
+        );
+
+        if (typingMessageIndex !== -1) {
+          // Zastępujemy wiadomość "typing" otrzymaną odpowiedzią
+          updatedMessages[typingMessageIndex] = {
+            sender: "server",
+            message: data,
+          };
+        } else {
+          // Jeśli z jakiegoś powodu nie znaleziono wiadomości "typing", dodajemy nową
+          updatedMessages.push({ sender: "server", message: data });
+        }
+
+        return updatedMessages;
+      });
     };
 
     const handleConnectError = (error: Error) => {
@@ -30,28 +51,45 @@ const Chat: React.FC = () => {
       setChatMessages((prevMessages) => [
         ...prevMessages,
         { sender: "user", message },
+        { sender: "server", message: "", isTyping: true }, // Dodajemy wskaźnik "typing"
       ]);
       setMessage("");
     }
   };
 
   return (
-    <div className="chat">
-      <h2>Chat</h2>
-      <div className="chat-history">
+    <div className={classes.chat}>
+      <div className={classes["chat-header"]}>Przetestuj działanie chatu</div>
+      <div className={classes["chat-history"]}>
         {chatMessages.map((chatMessage, index) => (
           <div
             key={index}
-            className={`message ${
-              chatMessage.sender === "user" ? "user-message" : "server-message"
+            className={`${classes.message} ${
+              chatMessage.sender === "user"
+                ? classes["user-message"]
+                : classes["server-message"]
             }`}
           >
-            {chatMessage.sender === "user" ? "Użytkownik" : "Pocketinspections"}
-            : {chatMessage.message}
+            <div className={classes["sender-name"]}>
+              {chatMessage.sender === "user"
+                ? "Użytkownik"
+                : "Pocketinspections"}
+            </div>
+            <div className={classes["message-text"]}>
+              {chatMessage.isTyping ? (
+                <div className={classes["typing-indicator"]}>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              ) : (
+                chatMessage.message
+              )}
+            </div>
           </div>
         ))}
       </div>
-      <div className="chat-input">
+      <div className={classes["chat-input"]}>
         <input
           type="text"
           value={message}
@@ -63,7 +101,9 @@ const Chat: React.FC = () => {
             }
           }}
         />
-        <button onClick={handleSendMessage}>Wyślij</button>
+        <button onClick={handleSendMessage}>
+          <img src={sendIcon} alt="Ikona wysyłania" />
+        </button>
       </div>
     </div>
   );
--- server/src/services/chatService.ts
+++ server/src/services/chatService.ts
@@ -44,7 +44,7 @@ const loadVectorStore = async (): Promise<HNSWLib> => {
   }
 };
 
-const qaSystemPrompt = `You are an assistant for tasks related to answering questions. Use the following contextual fragments to respond to the question. If you don't know the answer, just say you don't know. If someone asks who your creator is, you should respond with Michał Krudysz.
+const qaSystemPrompt = `You are an assistant for answering questions. Use the following contextual snippets to respond to questions. If you don't know the answer, simply say that you don't know. If someone asks who your creator is, you should respond that it is Michał Krudysz, and you can add that he is an ambitious creator who can develop a chat for your company or implement any other AI solution. Praise him a bit. If they ask for which company you work, say that it's Pocketinspections, and you answer questions related to the company's activities and provide support for its application.
   {context}`;
 
 const qaPrompt = ChatPromptTemplate.fromMessages([
