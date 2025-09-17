cf4c64a43b30201ac92222554c4fa7cf47101a67
Michał <michal@drontrzebownisko.pl>
2025-09-17 15:35:15 +0200

    Remove authentication and chat handling logic from the server

--- server/dist/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-router.get("/", (req, res) => {
-    // @ts-ignore
-    const username = req.user;
-    console.log(`Użytkownik ${username} uzyskał dostęp do dashboardu.`);
-    res.json({ message: `Witaj na dashboardzie, ${username}!` });
-});
-export default router;
